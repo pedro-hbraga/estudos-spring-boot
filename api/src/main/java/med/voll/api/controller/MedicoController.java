3f3139ff7d6403f93196b177e5f1ccdc9a1c170c
pedro-hbraga <braga-pedro@hotmail.com>
2023-04-22 14:43:27 -0300

    Conexao com banco de dados, Validacoes utilizando Bean Validation e Insert no BD

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    //INJECAO DE DEPENDENCIA - SPRING ira inicializar esse atributo
+    @Autowired //INJETANDO
+    private MedicoRepository repository;
+
     //METODO POST  - Captando dados do Body em JSON
     @PostMapping
-    public void cadastrarMedico(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional // do pack Spring, precisa de transacao ativa com BD
+    public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados){ //VEM DA REQUISICAO
+
+        //conversao de DTO para Medico
+        repository.save(new Medico(dados)); // INSERT NO BANCO
     }
 
 }
--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-//record -> seria o equivalente as Props do .NET, assim nao precisamos digitar Getters e Setters
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+//record -> seria o equivalente as Props do .NET, assim nao precisamos digitar Getters
+// record contem apenas atributos, construtor e metodos de leitura (GETTERS), ou seja, SEM COMPORTAMENTO
+public record DadosCadastroMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
 }
